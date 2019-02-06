4f1e4e60c379caead8c0296f5424e6681bb4cfe4
radoslawczapp <czappradoslaw@gmail.com>
2019-02-06 18:24:15 +0100

    Video Mode Added

--- js/main.js
+++ js/main.js
@@ -7,3 +7,23 @@ $(document).ready(function(){
 		$('#main_nav').removeClass('active');
 	});
 });
+
+function show_video(id){
+            var video = document.getElementById(id);
+			var content = document.getElementById('content');
+			var poster = document.getElementById('poster');
+            if (video.style.display == 'inline'){
+				 video.style.display = 'none';
+			} else{
+                video.style.display = 'inline';
+				video.autoplay = true;
+				video.load();
+				content.style.display = 'none';
+				poster.style.display = 'none';
+            }
+			$('video').on('ended',function(){
+				$(this).hide();
+				content.style.display = 'inline';
+				poster.style.display = 'inline';
+			});
+        }
